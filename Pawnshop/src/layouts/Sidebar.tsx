b0d1b933b4be5f897e7f4c9c53e1fe70eeabc406
Jakub Kliś <jakubklis2201@gmail.com>
2024-10-04 10:32:47 +0200

    feat: Add Pawnshop logo and Settings button to Sidebar

--- Pawnshop/src/layouts/Sidebar.tsx
+++ Pawnshop/src/layouts/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import PawnshopLogo from "../assets/PawnshopLogo.png";
 
 type SidebarProps = {
   isOpen: boolean;
@@ -7,77 +8,87 @@ type SidebarProps = {
 export default function Sidebar({ isOpen }: SidebarProps) {
   return (
     <div
-      className={`w-64 bg-gray-800 text-white h-screen fixed top-0 left-0 transform ${
+      className={`w-64 bg-gray-800 text-white h-screen fixed top-0 left-0 flex flex-col justify-between transform ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       } transition-transform duration-300 ease-in-out`}
     >
-      <nav className="p-4">
-        <ul>
-          <li>
-            <NavLink
-              to="/dashboard"
-              end
-              className={({ isActive }) =>
-                isActive
-                  ? "block p-2 bg-gray-700"
-                  : "block p-2 hover:bg-gray-600"
-              }
-            >
-              Dashboard
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/dashboard/add-customer"
-              end
-              className={({ isActive }) =>
-                isActive
-                  ? "block p-2 bg-gray-700"
-                  : "block p-2 hover:bg-gray-600"
-              }
-            >
-              Add new customer
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/dashboard/customers"
-              className={({ isActive }) =>
-                isActive
-                  ? "block p-2 bg-gray-700"
-                  : "block p-2 hover:bg-gray-600"
-              }
-            >
-              All Customers
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/dashboard/add-product"
-              end
-              className={({ isActive }) =>
-                isActive
-                  ? "block p-2 bg-gray-700"
-                  : "block p-2 hover:bg-gray-600"
-              }
-            >
-              Add new product
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/dashboard/products"
-              className={({ isActive }) =>
-                isActive
-                  ? "block p-2 bg-gray-700"
-                  : "block p-2 hover:bg-gray-600"
-              }
-            >
-              All Products
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div>
+        <div className="p-4 flex justify-center">
+          <img src={PawnshopLogo} alt="Pawnshop Logo" className="w-32 h-auto" />
+        </div>
+        <nav className="p-4">
+          <ul>
+            <li>
+              <NavLink
+                to="/dashboard"
+                end
+                className={({ isActive }) =>
+                  isActive
+                    ? "block p-2 bg-gray-700"
+                    : "block p-2 hover:bg-gray-600"
+                }
+              >
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dashboard/add-customer"
+                end
+                className={({ isActive }) =>
+                  isActive
+                    ? "block p-2 bg-gray-700"
+                    : "block p-2 hover:bg-gray-600"
+                }
+              >
+                Add new customer
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dashboard/customers"
+                className={({ isActive }) =>
+                  isActive
+                    ? "block p-2 bg-gray-700"
+                    : "block p-2 hover:bg-gray-600"
+                }
+              >
+                All Customers
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dashboard/add-product"
+                end
+                className={({ isActive }) =>
+                  isActive
+                    ? "block p-2 bg-gray-700"
+                    : "block p-2 hover:bg-gray-600"
+                }
+              >
+                Add new product
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dashboard/products"
+                className={({ isActive }) =>
+                  isActive
+                    ? "block p-2 bg-gray-700"
+                    : "block p-2 hover:bg-gray-600"
+                }
+              >
+                All Products
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="p-4">
+        <button className="w-full py-2 bg-gray-700 hover:bg-gray-600 rounded text-white">
+          Settings
+        </button>
+      </div>
     </div>
   );
 }
