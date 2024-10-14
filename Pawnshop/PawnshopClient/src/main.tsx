5c5b6d0d96d84d1c8d58aaec8bfc202798cf35ba
Jakub Kliś <jakubklis2201@gmail.com>
2024-10-14 14:57:15 +0200

    chore: Move index.css to styles folder

--- Pawnshop/PawnshopClient/src/main.tsx
+++ Pawnshop/PawnshopClient/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import "./styles/index.css";
 import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
